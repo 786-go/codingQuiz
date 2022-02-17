d1f6e1ee0370c6d90e8f3a1a582e282d2eaff65f
786-go <noorullahw@gmail.com>
2022-02-17 15:18:41 -0800

    timer added

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 var bgnQuiz = document.querySelector("#btn")
-
-var startBtnHandler = function() {
+var quizArea = document.getElementById("quizarea")
+quizArea.style.display = "none"
+var timer = 120;
+var timerObject;
+var counter = document.getElementById("counter")
+var startBtnHandler = function () {
     bgnQuiz.style.display = "none"
-}
+    quizArea.style.display = "block"
+    timerObject = setInterval(function () {
+        counter.textContent = "time left" + timer
+        
+        if(timer > 1) {
+            timer--;
+        }else {
+            clearInterval(timerObject)
+        }
+    },1000)
+};
 
 
 
 
-
+// var questionlist = [ 
+//     {
+//         q:"",
+//         c:["o1","o2","o3","o4"]
+//         a:4
+//     }
+// ]
 
 
 
