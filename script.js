3c767e2ca251c8407e406506565694f880cb0adf
786-go <noorullahw@gmail.com>
2022-02-17 14:35:53 -0800

    button appear/disappear

---
+++ script.js
@@ -0,0 +1,33 @@
+var bgnQuiz = document.querySelector("#btn")
+
+var startBtnHandler = function() {
+    bgnQuiz.style.display = "none"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bgnQuiz.addEventListener("click", startBtnHandler)
